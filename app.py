f2f429b7fb00bb0540eb24eceab968917ce7911d
eliasziegeler-droid <elias.ziegeler@icloud.com>
2026-08-03 13:02:29 +1200

    app.py+connect and query today

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, g 
+import sqlite3 
+
+
+DATABASE = 'database.db'
+
+
+
+
+#initialise app
+app = flask(__name__)
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+@app.route('/')
+def home()
+    #homepage
+    db = get_db()
+    cursor = db.cursor()
+    sql = "SELECT * FROM Bikes;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    return str(results)
+
+if __name__ == " __main__":
+    app.run(debug=True)
