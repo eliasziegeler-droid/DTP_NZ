95b1f5857dd4cf0a653ecc158c35d81763c8d534
eliasziegeler-droid <elias.ziegeler@icloud.com>
2026-08-04 12:48:52 +1200

    images and acceleration today- seeing resluts

--- app.py
+++ app.py
@@ -8,10 +8,11 @@ DATABASE = 'database.db'
 
 
 #initialise app
-app = flask(__name__)
+app = Flask(__name__)
+
 
 def get_db():
-    db = getattr(g, '_database', None)
+    db = getattr(g,'_database', None)
     if db is None:
         db = g._database = sqlite3.connect(DATABASE)
     return db
@@ -22,15 +23,17 @@ def close_connection(exception):
     if db is not None:
         db.close()
 
+
 @app.route('/')
-def home()
+def home():
     #homepage
     db = get_db()
     cursor = db.cursor()
-    sql = "SELECT * FROM Bikes;"
+    sql = "SELECT * FROM Motorbikes;"
     cursor.execute(sql)
     results = cursor.fetchall()
     return str(results)
 
-if __name__ == " __main__":
+
+if __name__ == "__main__":
     app.run(debug=True)
